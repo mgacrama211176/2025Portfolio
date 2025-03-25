44bfeebd29a649ee931b15bc0471ebda2245834d
marlonG <maruronu@gmail.com>
2025-03-26 02:51:36 +0800

    Added more data for Exp

--- src/app/components/Experience.tsx
+++ src/app/components/Experience.tsx
@@ -76,6 +76,10 @@ const Experience = () => {
                   </span>
                   <h3 className="text-2xl md:text-3xl font-bold mb-4 text-white">
                     {item.heading}
+                    <br />
+                    <span className="text-gray-400 text-sm font-normal">
+                      ( {item.company} )
+                    </span>
                   </h3>
                   <p className="text-gray-300 leading-relaxed">
                     {item.description}
